f6c868a6c2542d3773bf7008652e20ae14527ea0
Vincent Li <vincentli774@gmail.com>
2025-09-07 16:28:01 +0200

    fourth commit

--- weather-be/app.js
+++ weather-be/app.js
@@ -1,22 +1,34 @@
+//use express, cors, and body-parser to simplify
+//the request handling
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
+//using cors allows the front end to make requests 
+// for cities to the backend
 app.use(cors())
+//body-parser parses incoming requests making them 
+//available as objects
 app.use(bodyParser.json());
 let cities = []; // In-memory storage for cities
 
+//get route for cities
 app.get("/cities", (req, res) => {
   res.json(cities);
 });
 
+//post route for cities
 app.post("/cities", (req, res) => {
+  //accessing city key value in json body
   const city = req.body["city"];
+  //adding city value to cities in-memory storage
   cities.push(city);
+  //the 201 code indicates that the city has been added
   res.status(201).json(city);
 });
   
+//lets the server run on port 300
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
