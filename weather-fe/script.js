337fe08a55da31c799f114d47d4885764e0fc08d
Vincent Li <vincentli774@gmail.com>
2025-09-08 11:50:10 +0200

    fifth commit

--- weather-fe/script.js
+++ weather-fe/script.js
@@ -1,19 +1,19 @@
 //use async function as we need to wait for response from OpenWeatherMap 
 async function getWeather() {
-  //get city from html input
+  //get city from user input
   const city = document.getElementById('city').value;
   const apiKey = '07db5ee3e129a96e70de045f61343790'; // OpenWeatherMap API key
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   //fetch weather data for required city
   const res = await fetch(url);
-  //convert response into json
+  //convert response into json object
   const data = await res.json();
   //const json = JSON.stringify(data);
 
   //200 is the code in the response that means the city is recognized
   if (data.cod == 200){
 //document.getElementById("result").innerHTML = json;
-//extracting data from json, converting to string, and returning data in html document
+//extracting data from json object, converting to string, and returning data in html document
 document.getElementById("temperature").innerHTML = JSON.stringify(data.main.temp);
 document.getElementById("weather").innerHTML = JSON.stringify(data.weather[0].main);
 document.getElementById("windspeed").innerHTML = JSON.stringify(data.wind.speed);
